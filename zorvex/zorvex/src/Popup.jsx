b3b310ca8d4c6223880b05652f052b23ca45214a
ZorvexHealth <info@zorvexhealthcare.com>
2026-06-14 15:25:00 +0530

    POPUP HEADING CHANGE

--- zorvex/zorvex/src/Popup.jsx
+++ zorvex/zorvex/src/Popup.jsx
@@ -454,7 +454,7 @@ function Popup({ closePopup }) {
                 <div className="popup-tag">GET STARTED</div>
 
                 <div className="popup-title-row">
-                  <h1 className="popup-title">One Claim, at a Time.</h1>
+                  <h1 className="popup-title">Smarter Revenue, Better Results.</h1>
 
                   <img src={singleLeaf} alt="" className="single-leaf mb-4" />
                 </div>
